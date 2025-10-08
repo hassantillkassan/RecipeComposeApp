8dc839bea4f0905bfc7321538f73573f21b22c01
Андрей <@jay_gatz>
2025-10-08 23:31:34 +0700

    RCA-05:Подключение и настройка кастомных шрифтов

---
+++ app/src/main/java/com/example/recipecomposeapp/ui/theme/FontFamily.kt
@@ -0,0 +1,16 @@
+package com.example.recipecomposeapp.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.example.recipecomposeapp.R
+
+val montserratFontFamily = FontFamily(
+    Font(R.font.montserrat_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.montserrat_medium, FontWeight.Medium, FontStyle.Normal),
+)
+
+val montserratAlternatesFontFamily = FontFamily(
+    Font(R.font.montserrat_alternates_semi_bold, FontWeight.SemiBold, FontStyle.Normal)
+)

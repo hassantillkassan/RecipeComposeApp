343806981044529fd66ff82ea489479ed4ead1b7
Андрей <@jay_gatz>
2025-10-31 21:03:11 +0700

    RCA-13: Создание UI-модели CategoryUiModel

--- app/src/main/java/com/example/recipecomposeapp/data/model/RecipeDto.kt
+++ app/src/main/java/com/example/recipecomposeapp/data/model/RecipeDto.kt
@@ -2,8 +2,10 @@ package com.example.recipecomposeapp.data.model
 
 data class RecipeDto(
     val id: Int,
+    val categoryId: Int,
     val title: String,
     val ingredients: List<IngredientDto>,
     val method: List<String>,
     val imageUrl: String,
+    val isFavorite: Boolean,
 )
